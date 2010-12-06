11abdac7df7cb89eba09e53270e8464c6c2cd849
Jar.Patryk <Jar.Patryk@cc326e5e-ded0-8dac-5eec-cc32ff541322>
2010-12-06 14:36:20 +0000

    dodanie testow

--- 1/klasy-oop/test/Funkcje.test.php
+++ 1/klasy-oop/test/Funkcje.test.php
@@ -4,15 +4,66 @@ require_once '../struktury.php';
 
 class StackTest extends PHPUnit_Framework_TestCase
 {
-    public function testPushAndPop()
-    {
-        $stack = array();
-        $this->assertEquals(0, count($stack));
-        array_push($stack, 'foo');
-        $this->assertEquals('foo', $stack[count($stack)-1]);
-        $this->assertEquals(1, count($stack));
-        $this->assertEquals('foo', array_pop($stack));
-        $this->assertEquals(0, count($stack));
-    }
+	private function getKwadrat( $bok )
+	{
+		$kwadrat = new Kwadrat();
+		$kwadrat->bok = $bok;
+
+		return $kwadrat;
+	}
+
+	private function getTrojkat( $podstawa, $wysokosc )
+	{
+		$trojkat = new Trojkat();
+		$trojkat->podstawa = $podstawa;
+		$trojkat->wysokosc = $wysokosc;
+
+		return $trojkat;
+	}
+	
+	public function testPoleKwadratuPoprawneDane()
+	{
+		$bok = 10;
+
+		$obj = $this->getKwadrat($bok);
+		$this->assertEquals($bok*$bok, Funkcje::poleKwadratu($obj));
+	}
+
+	public function testPoleKwadratuZeroBok()
+	{
+		$bok = 0;
+
+		$obj = $this->getKwadrat($bok);
+		$this->assertEquals($bok*$bok, Funkcje::poleKwadratu($obj));
+	}
+
+	public function testPoleTrojkata()
+	{
+		$h = 10;
+		$pod = 5;
+
+		$obj = $this->getTrojkat($pod, $h);
+		$this->assertEquals((1/2)*($h*$pod), Funkcje::poleTrojkata($obj));
+	}
+
+	public function testPoleTrojkataZeroPodst()
+	{
+		$h = 10;
+		$pod = 0;
+
+		$obj = $this->getTrojkat($pod, $h);
+		$this->assertEquals((1/2)*($h*$pod), Funkcje::poleTrojkata($obj));
+	}
+
+	public function testPoleTrojkataZeroWys()
+	{
+		$h = 0;
+		$pod = 6;
+
+		$obj = $this->getTrojkat($pod, $h);
+		$this->assertEquals((1/2)*($h*$pod), Funkcje::poleTrojkata($obj));
+	}
 }
+
+
 ?>
